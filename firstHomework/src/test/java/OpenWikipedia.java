71b4f654f2bbc0957f504dfbd619fc8e7e90c9f8
Rubina <rr.abajyan@gmail.com>
2019-01-22 08:34:10 +0200

    Created module "First project"

---
+++ firstHomework/src/test/java/OpenWikipedia.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class OpenWikipedia {
+    WebDriver wd;
+
+    @BeforeClass
+
+    public void setUp(){
+        wd = new ChromeDriver();
+    }
+
+    @Test
+    public void wikiBrowserChrome(){
+        wd.get("https://www.wikipedia.org/");
+    }
+
+    @AfterClass
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        wd.quit();
+    }
+}
+
